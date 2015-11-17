a4b3cde912fe0056548b5bac436657b8430af671
Peter Winterhof <PeterWinterhof@Peters-MacBook-Pro.local>
2015-11-17 09:44:37 -0500

    conflicts

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var things = [1,2,3,4,5]
+var things = [1,2,3,4]
 
 for (var i = 0; i < array.length; i++) {
 	console.log(things[i])
 };
 
-for (var i = 0; i < 3; i++) {
-	console.log("This is your number: " + things[i])
+for (var i = 0; i < 8; i++) {
+	console.log("This is your number: " + (things[i]+8)
 };
