aff5cdc69ea1ab8f4650463d7bcdfe24e83a8166
Peter Winterhof <PeterWinterhof@Peters-MacBook-Pro.local>
2015-11-17 09:40:54 -0500

    third push

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var things = [1,2,3,4,5]
+var things = [1,2,3,4,5,6,7]
 
 for (var i = 0; i < array.length; i++) {
-	console.log(things[i])
+	console.log(things[i] + "is not your number")
 };
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 4; i++) {
 	console.log("This is your number: " + things[i])
 };
+
