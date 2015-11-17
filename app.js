d0a7269c83328cc6c51b4fc4b2534480a95c96cb
Peter Winterhof <PeterWinterhof@Peters-MacBook-Pro.local>
2015-11-17 09:26:58 -0500

    second push

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var things = [1,2,3,4,5]
 for (var i = 0; i < array.length; i++) {
 	console.log(things[i])
 };
+
+for (var i = 0; i < 3; i++) {
+	console.log("This is your number: " + things[i])
+};
